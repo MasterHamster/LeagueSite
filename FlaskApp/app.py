f4822bee6834463de19b54784804f54631a93e42
ZaRa <travian014@walla.com>
2016-04-29 00:35:29 +0300

    Added css files into static directory.

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_restful import Resource, Api
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+# url_for('static', filename='style.css')
 
 if __name__ == '__main__':
     app.run()
